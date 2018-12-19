fba062332df3d48e3bd43bf54b20c65f340e2cb6
Kristján Pétursson <kristjan@gmail.com>
2018-12-18 22:09:26 -0800

    Day 19
    The program when initialized to 0 generates the number 898, then
    proceeds to find its factors and add them together. Initialized to 1, it
    generates 10551298 instead of 898, then does the same.

---
+++ 2018/19/op.rb
@@ -0,0 +1,149 @@
+input = File.readlines(ARGV[0]).each(&:strip!)
+
+class Op
+  attr_reader :registers
+
+  def initialize(registers)
+    @registers = registers.dup
+  end
+
+  def perform!(a, b, save)
+    registers[save] = run(a, b)
+  end
+
+  def register(r)
+    registers[r]
+  end
+
+  def immediate(i)
+    i
+  end
+
+  def nop(n)
+    nil
+  end
+end
+
+class Addr < Op
+  def run(a, b)
+    registers[a] + registers[b]
+  end
+end
+
+class Addi < Op
+  def run(a, b)
+    registers[a] + b
+  end
+end
+
+class Mulr < Op
+  def run(a, b)
+    registers[a] * registers[b]
+  end
+end
+
+class Muli < Op
+  def run(a, b)
+    registers[a] * b
+  end
+end
+
+class Banr < Op
+  def run(a, b)
+    registers[a] & registers[b]
+  end
+end
+
+class Bani < Op
+  def run(a, b)
+    registers[a] & b
+  end
+end
+
+class Borr < Op
+  def run(a, b)
+    registers[a] | registers[b]
+  end
+end
+
+class Bori < Op
+  def run(a, b)
+    registers[a] | b
+  end
+end
+
+class Setr < Op
+  def run(a, b)
+    registers[a]
+  end
+end
+
+class Seti < Op
+  def run(a, b)
+    a
+  end
+end
+
+class Gtir < Op
+  def run(a, b)
+    a > registers[b] ? 1 : 0
+  end
+end
+
+class Gtri < Op
+  def run(a, b)
+    registers[a] > b ? 1 : 0
+  end
+end
+
+class Gtrr < Op
+  def run(a, b)
+    registers[a] > registers[b] ? 1 : 0
+  end
+end
+
+class Eqir < Op
+  def run(a, b)
+    a == registers[b] ? 1 : 0
+  end
+end
+
+class Eqri < Op
+  def run(a, b)
+    registers[a] == b ? 1 : 0
+  end
+end
+
+class Eqrr < Op
+  def run(a, b)
+    registers[a] == registers[b] ? 1 : 0
+  end
+end
+
+program = input.map do |line|
+  op, a, b, c = line.split(' ')
+  a, b, c = [a, b, c].map(&:to_i)
+  [op, a, b, c]
+end
+
+ipline = program.shift
+ipr = ipline[1]
+
+init = ARGV[1].to_i || 0
+registers = [init, 0, 0, 0, 0, 0]
+count = 0
+while registers[ipr] >= 0 && registers[ipr] < program.size
+  print "#{count}\r" if count % 100 == 0
+  #before = registers.dup
+  opcode, a, b, c = program[registers[ipr]]
+  op_class = Op.const_get(opcode.capitalize)
+  op = op_class.new(registers)
+  op.perform!(a, b, c)
+  registers = op.registers
+  #puts [*before, '|', opcode, a, b, c, '|', *registers].inspect
+  registers[ipr] += 1
+  count += 1
+end
+puts
+
+puts registers.inspect
